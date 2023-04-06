55851279f691191d6e0d1def63d7ce31ae792180
Lucas Aires <106110567+Aireszzzzz@users.noreply.github.com>
2023-04-06 08:23:48 -0300

    fazendo

--- aula12ex2/script.js
+++ aula12ex2/script.js
@@ -2,4 +2,8 @@ function verificar () {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = window.document.getElementById('txtano')
+    var res = window.document.getElementById('res')
+    if (fano.ariaValueMax.length == 0 || fano.value > ano) {
+        window.alert('Verifique os dados e tente novamente')
+    }
 }
